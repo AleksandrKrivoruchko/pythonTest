dab7c733f640c570f28e7f597533b77629c75f57
AleksandrKrivoruchko <avk741120@yandex.ru>
2022-03-03 14:06:55 +0300

    Added file task2

---
+++ testPython/task2.py
@@ -0,0 +1,29 @@
+def triangle(x):
+    start = x//2 + 1
+    end = start
+    symbol = '*'
+    transfer = '\n'
+    str_triangle = ''
+    while start:
+        str_triangle += symbol_print(0, start, ' ')
+        str_triangle += symbol
+        str_triangle += symbol_print(start, end, symbol)
+        str_triangle += transfer
+        start -= 1
+        end += 1
+    return str_triangle
+
+def symbol_print(count, end_space, symbol):
+    str_temp = ''
+    while count < end_space:
+        str_temp += symbol
+        count +=1
+    return str_temp
+
+
+while 1:
+    len = int(input("Введите число (чтобы выйти введите 0)\n"))
+    if not len:
+        break
+    str = triangle(len)
+    print(str)
