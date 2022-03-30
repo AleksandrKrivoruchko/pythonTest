5300adba6182c4bed79f7ecccd0d289b2e646ccc
AleksandrKrivoruchko <avk741120@yandex.ru>
2022-03-30 21:46:48 +0300

    Modified file file_test.py

--- testPython/file_test.py
+++ testPython/file_test.py
@@ -1,7 +1,12 @@
 # Работа с файлами
+import sys
 file_index = open('file_test.py', 'r')
 file_value = file_index.read()
 print(type(file_value))
 print(file_value)
 res_split = file_value.split()
 print(res_split)
+
+sys.stdout.write('Вывод с помощью потока вывода\n')
+value_in = sys.stdin.readline()
+print(value_in)
