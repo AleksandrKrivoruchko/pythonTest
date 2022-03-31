7aae5564fec0633fa4cc576f28094ebc9ac733c8
AleksandrKrivoruchko <avk741120@yandex.ru>
2022-03-31 17:23:50 +0300

    Fixed bugs

--- testPython/task19_s4_h_w.py
+++ testPython/task19_s4_h_w.py
@@ -1,12 +1,12 @@
 # Реализовать алгоритм перемешивания списка
 import random
 
-test_list1 = [i for i in range(1, 31)]
+test_list1 = [i for i in range(1, 16)]
 print(test_list1)
 test_list = [test_list1[i] for i in range(len(test_list1))]
 while test_list[0] == test_list1[0]:
-    for i in range(random.randint(10, 100)):
-        mixing_list = [random.randint(0, len(test_list) - 1) for i in range(len(test_list))]
+    for k in range(random.randint(10, 100)):
+        mixing_list = [random.randint(0, len(test_list) - 1) for j in range(len(test_list))]
         # print(mixing_list)
         length = len(mixing_list) // 2
         for i in range(length):
