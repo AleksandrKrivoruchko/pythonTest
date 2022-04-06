a8643b37414b096b5a24525be62f738517323e61
AleksandrKrivoruchko <avk741120@yandex.ru>
2022-04-06 16:47:21 +0300

    Added layout for solving task 8

---
+++ tasks_homewok8/module_input.py
@@ -0,0 +1,8 @@
+# Модуль ввода
+from module_output import demo_mod as demo
+def do_mod(count):
+    if not count:
+        demo()
+
+
+do_mod(0)
---
+++ tasks_homewok8/module_output.py
@@ -0,0 +1,25 @@
+# Модуль для вывода
+# print('0 | X | 0')
+# print('---------')
+# print('X | 0 | X')
+# print('---------')
+# print('0 | X | 0')
+board_list = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+
+
+def do_mod(board_list):
+    print(f'{board_list[0]} | {board_list[1]} | {board_list[2]}')
+    print('---------')
+    print(f'{board_list[3]} | {board_list[4]} | {board_list[5]}')
+    print('---------')
+    print(f'{board_list[6]} | {board_list[7]} | {board_list[8]}')
+
+
+def demo_mod():
+    print('''\033[32m                            Игра <<Крестики-нолики>>                       
+    1   2   3           Введено X (1, 3)        Введено 0 (3, 1)
+1   0 | X | 0             |   | X                 |   |
+    ---------           ---------               ---------
+2   0 | X | X             |   |                   |   |
+    ---------           ---------               ---------
+3   X | X | 0             |   |                 0 |   |''')
