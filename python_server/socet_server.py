91983dfcac6cfe609771db8059cdd78da29fc052
AleksandrKrivoruchko <avk741120@yandex.ru>
2022-09-27 17:27:46 +0300

    Experiment with server

--- python_server/socet_server.py
+++ python_server/socet_server.py
@@ -1,20 +1,60 @@
 import socket
+import select
 from operator import mul
 from functools import reduce
+from collections import namedtuple
+from sqlite3 import connect
+
+Session = namedtuple('Session', ['address', 'file'])
+
+sessions = {}
+callback = {}
+generators = {}
 
 
 def reactor(host, port):
     sock = socket.socket()
     sock.bind((host, port))
     sock.listen(5)
+    sock.setblocking(0)
+
+    sessions[sock] = None
     print(f'Server up, running, and waiting for call on {host}:{port}')
 
     try:
         while True:
-            conn, cli_address = sock.accept()
-            process_request(conn, cli_address)
+            ready_to_read, _, _ = select.select(sessions, [], [], 0.1)
+            for conn in ready_to_read:
+                if conn is sock:
+                    conn, cli_address = sock.accept()
+                    connect(conn, cli_address)
+                    continue
+
+                line = sessions[conn].file.readline()
+                if line:
+                    callback[conn][conn, line.rstrip()]
+                else:
+                    disconnect(conn)
     finally:
         sock.close()
+
+
+def connnect(conn, cli_address):
+    sessions[conn] = Session(cli_address, conn.makefile())
+
+    gen = process_request(conn)
+    generators[conn] = gen
+    callback[conn] = gen.send(None)
+
+
+def disconnect(conn):
+    gen = generators.pop(conn)
+    gen.close()
+    sessions[conn].file.close()
+    conn.close()
+
+    del sessions[conn]
+    del callback[conn]
 
 
 def process_request(conn, cli_address):
